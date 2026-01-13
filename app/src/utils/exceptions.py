6671074663aaaf9e37c7cf8e03ce6bf646f54c41
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-13 19:07:12 +0500

    Add exceptions for otp

--- app/src/utils/exceptions.py
+++ app/src/utils/exceptions.py
@@ -17,3 +17,13 @@ class InvalidCredentialsException(HTTPException):
             detail="Incorrect email or password",
             headers={"Authenticate": "Bearer"},
         )
+
+
+class OTPNotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail="OTP not found for this user")
+      
+        
+class InvalidOTPException(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid OTP provided")
