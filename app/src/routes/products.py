69023accfa52a84d5943d94f566a8f1bea1883cc
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-26 21:52:41 +0500

    Add routers update and delete product

--- app/src/routes/products.py
+++ app/src/routes/products.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from app.src.db.database import get_db
 from app.src.models.user import User as UserModel
 from app.src.core.security import required_role
-from app.src.schemas.products import Product as ProductSchema
+from app.src.schemas.products import Product as ProductSchema, ProductUpdate
 from app.src.models.products import Product as ProductModel
 
 router = APIRouter(tags=['Products'])
@@ -35,4 +35,39 @@ def add_product(product: ProductSchema, db: Session = Depends(get_db), current_u
     db.commit()
     db.refresh(new_product)
 
-    return new_product
+    return {'message': 'Product added successfully'}
+
+
+@router.put('/admin/update-product/{product_id}')
+def update_product(product_id: int, product: ProductSchema, db: Session = Depends(get_db), current_user: dict = Depends(required_role('admin'))):
+    existing_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+    if not existing_product:
+        return {'message': 'Product not found'}
+
+    existing_product.name = product.name
+    existing_product.description = product.description
+    existing_product.price = product.price
+    existing_product.category = product.category
+    existing_product.stock_quantity = product.stock_quantity
+
+    db.add(existing_product)
+    db.commit()
+    db.refresh(existing_product)
+
+    return {'message': 'Product updated successfully'}
+
+
+
+@router.delete('/admin/delete-product/{product_id}')
+def delete_product(product_id: int, db: Session = Depends(get_db), current_user: dict = Depends(required_role('admin'))):
+    existing_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+    if not existing_product:
+        return {'message': 'Product not found'}
+
+    db.delete(existing_product)
+    db.commit()
+
+    return {'message': 'Product deleted successfully'}
+    
