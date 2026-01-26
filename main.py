b0e916c80f30a991a1d955fd25ac1b73ba591252
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-26 18:17:54 +0500

    add product route

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 
 from app.src.db.database import Base, engine
-from app.src.routes.user import router as user_routes
 from app.src.core.security import create_default_admin
+from app.src.routes.user import router as user_routes
+from app.src.routes.products import router as product_routes
 
 
 @asynccontextmanager
@@ -19,7 +20,7 @@ def home():
     return {"message": "Welcome to the E-Commerce Platform API"}
 
 app.include_router(user_routes, prefix="/user", tags=["User"])
-
+app.include_router(product_routes)
 
 if __name__ == "__main__":
     import uvicorn
