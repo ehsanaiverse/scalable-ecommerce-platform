1f3478c1b221892d7d241f38ceee1ed7e0c4f40e
Ehsan Ullah <ihsan42180@gmail.com>
2026-01-29 22:43:13 +0500

    add to handle image upload, partial update, and cleanup on delete

--- app/src/routes/products.py
+++ app/src/routes/products.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, HTTPException
+import os
+import uuid
+from fastapi import APIRouter, Depends, HTTPException, Form, UploadFile, File
 from sqlalchemy.orm import Session
 from app.src.db.database import get_db
 from app.src.models.user import User as UserModel
@@ -10,6 +12,9 @@ from app.src.models.inventory import Inventory as InventoryModel
 
 router = APIRouter(tags=['Products'])
 
+PRODUCT_IMAGES_PATH = "app/static/product_images"
+os.makedirs(PRODUCT_IMAGES_PATH, exist_ok=True)
+
 @router.get('/products', response_model=list[ProductResponse])
 def get_products(db: Session = Depends(get_db)):
     products = db.query(ProductModel).all()
@@ -19,21 +24,46 @@ def get_products(db: Session = Depends(get_db)):
 
 
 @router.post('/admin/add-product', response_model=ProductResponse)
-def add_product(product: ProductCreate, db: Session = Depends(get_db), current_user: dict = Depends(required_role('admin'))):
-    # Check if category exists
-    category_check = db.query(CategoryModel).filter(CategoryModel.id == product.category_id).first()
+def add_product(
+    name: str = Form(...),
+    description: str | None = Form(None),
+    price: float = Form(...),
+    category_id: int = Form(...),
+    stock_quantity: int = Form(...),
+    image: UploadFile = File(...),
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(required_role('admin'))
+):
+    # Check category 
+    category_check = db.query(CategoryModel).filter(CategoryModel.id == category_id).first()
     if not category_check:
         raise HTTPException(status_code=400, detail='Category not found')
 
-    existing_product = db.query(ProductModel).filter(ProductModel.name == product.name).first()
+     # Check duplicate product
+    existing_product = db.query(ProductModel).filter(ProductModel.name == name).first()
     if existing_product:
         raise HTTPException(status_code=400, detail='Product with this name already exists')
 
+
+    # save image
+    file_ext = image.filename.split('.')[-1]
+    file_name = f"{uuid.uuid4()}.{file_ext}"
+    file_path = os.path.join(PRODUCT_IMAGES_PATH, file_name)
+    
+    
+    with open(file_path, 'wb') as f:
+        f.write(image.file.read())
+        
+    
+    image_path = f"/static/product_images/{file_name}"
+
+    
     new_product = ProductModel(
-        name=product.name,
-        description=product.description,
-        price=product.price,
-        category_id=product.category_id
+        name=name,
+        description=description,
+        price=price,
+        category_id=category_id,
+        image_path = image_path
     )
 
     db.add(new_product)
@@ -43,20 +73,27 @@ def add_product(product: ProductCreate, db: Session = Depends(get_db), current_u
     # Initialize Inventory
     new_inventory = InventoryModel(
         product_id=new_product.id,
-        stock_quantity=product.stock_quantity
+        stock_quantity=stock_quantity
     )
     db.add(new_inventory)
     db.commit()
 
     return new_product
 
+
+
 @router.put('/admin/update-product/{product_id}', response_model=ProductResponse)
 def update_product(
     product_id: int,
-    product: ProductUpdate,
+    name: str | None = Form(None),
+    description: str | None = Form(None),
+    price: str | None = Form(None),
+    category_id: str | None = Form(None),
+    image: UploadFile | None = File(None),
     db: Session = Depends(get_db),
     current_user: dict = Depends(required_role('admin'))
 ):
+    # Fetch product
     existing_product = db.query(ProductModel).filter(
         ProductModel.id == product_id
     ).first()
@@ -64,32 +101,38 @@ def update_product(
     if not existing_product:
         raise HTTPException(status_code=404, detail='Product not found')
 
-    update_data = product.model_dump(exclude_unset=True)
-
-    if not update_data:
-        raise HTTPException(
-            status_code=400,
-            detail="No fields provided for update"
-        )
-
-    if "category_id" in update_data and update_data["category_id"] is not None:
+    # Validate category if provided
+    if category_id is not None:
         category = db.query(CategoryModel).filter(
-            CategoryModel.id == update_data["category_id"]
+            CategoryModel.id == category_id
         ).first()
 
         if not category:
-            raise HTTPException(
-                status_code=400,
-                detail="Invalid category_id"
-            )
+            raise HTTPException(status_code=400, detail="Invalid category_id")
 
-    for field, value in update_data.items():
-        if value is None:
-            continue
-        if isinstance(value, str) and value.strip() == "":
-            continue
-        setattr(existing_product, field, value)
+    # Update simple fields
+    if name is not None and name.strip() !="":
+        existing_product.name = name
 
+    if description is not None and description.strip() != "":
+        existing_product.description = description
+
+    if price is not None and price.strip() != "":
+        existing_product.price = float(price)
+
+
+    if category_id is not None and category_id.strip() != "":
+        existing_product.category_id = category_id
+
+    # Handle image update
+    if image:
+        filename = f"{uuid.uuid4()}_{image.filename}"
+        file_path = os.path.join(PRODUCT_IMAGES_PATH, filename)
+
+        with open(file_path, "wb") as f:
+            f.write(image.file.read())
+
+        existing_product.image_path = f"/static/product_images/{filename}"
 
     db.commit()
     db.refresh(existing_product)
@@ -97,23 +140,37 @@ def update_product(
     return existing_product
 
 
+
+
 @router.delete('/admin/delete-product/{product_id}')
-
-def delete_product(product_id: int, db: Session = Depends(get_db), current_user: dict = Depends(required_role('admin'))):
-
-    existing_product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
-
-    if not existing_product:
+def delete_product(
+    product_id: int,
+    db: Session = Depends(get_db),
+    current_user: dict = Depends(required_role('admin'))
+):
+    product = db.query(ProductModel).filter(ProductModel.id == product_id).first()
+    if not product:
         raise HTTPException(status_code=404, detail='Product not found')
 
-    inventory = db.query(InventoryModel).filter(InventoryModel.product_id == product_id).first()
+    # Delete inventory
+    inventory = db.query(InventoryModel).filter(
+        InventoryModel.product_id == product_id
+    ).first()
 
     if inventory:
         db.delete(inventory)
 
+    # Delete image file from disk
+    if product.image_path:
+        # "/static/product_images/x.webp" → "app/static/product_images/x.webp"
+        file_path = product.image_path.lstrip("/")
+        file_path = os.path.join("app", file_path)
 
-    db.delete(existing_product)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    # Delete product
+    db.delete(product)
     db.commit()
 
-    return {'message': 'Product deleted successfully'}
-    
+    return {"message": "Product deleted successfully"}
